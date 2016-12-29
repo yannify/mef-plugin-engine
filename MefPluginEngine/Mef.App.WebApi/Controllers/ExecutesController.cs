17a05830d8c37176de63a0f7e54bc0a4f8eeeb93
Yanni Mitis <ymitis@gmail.com>
2016-12-28 20:46:51 -0800

    initial commit of solution skeleton

---
+++ MefPluginEngine/Mef.App.WebApi/Controllers/ExecutesController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Mef.App.WebApi.Controllers
+{
+    public class ExecutesController : ApiController
+    {
+        // GET: api/Executes
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/Executes/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Executes
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/Executes/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Executes/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ MefPluginEngine/Mef.App.WebApi/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Mef.App.WebApi.WebApiApplication" Language="C#" %>
