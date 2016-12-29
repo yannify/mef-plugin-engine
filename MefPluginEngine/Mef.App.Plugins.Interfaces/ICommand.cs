fbfaab1b82899d8fdbe464a9206bf59db871d397
Yanni Mitis <ymitis@gmail.com>
2016-12-28 20:53:06 -0800

    adding interfaces

---
+++ MefPluginEngine/Mef.App.Plugins.Interfaces/ICommand.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mef.App.Plugins.Interfaces
+{
+    public interface ICommand
+    {
+        ExecutionResult Execute(IDictionary<string, object> input);
+    }
+}
